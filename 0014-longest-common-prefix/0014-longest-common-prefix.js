4258f1ae36d10e003eb83cde6b47180b257f5a24
Momanyi <hassanmoses125@gmail.com>
2022-11-10 10:30:52 +0300

    Time: 99 ms (62.29%), Space: 43 MB (37.98%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -4,11 +4,11 @@
  */
 var longestCommonPrefix = function(strs) {
     if (!strs.length) return "";
-    
     for (let i = 0; i <=strs[0].length; i++){
-        if (!strs.every((string) => string[i] === strs[0][i])){
-            
-            return strs[0].slice(0 , i)
+        for (let k =1; k<strs.length; k++){
+            if (strs[0][i] !== strs[k][i]){
+                return strs[0].slice(0, i);
+            }
         }
     }
     return strs[0];
